efd9538f29d1d633706c525f0613601f14363ac1
Matt Howlett <matt.howlett@gmail.com>
2019-12-13 17:11:23 -0800

    main loop nearly there

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
 mod log_utils;
 mod hyper_tls;
 
+extern crate log;
+extern crate clap;
+extern crate futures;
+extern crate rdkafka;
+extern crate rdkafka_sys;
+
 use clap::{App, Arg};
-use log::info;
+
+use rdkafka::client::ClientContext;
+use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
+use rdkafka::consumer::stream_consumer::StreamConsumer;
+use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext, Rebalance};
+use rdkafka::error::KafkaResult;
+use rdkafka::message::{Headers, Message};
 use rdkafka::util::get_rdkafka_version;
+use futures::compat::Stream01CompatExt;
+use crate::futures::StreamExt;
+
+use log::{info, warn};
 use crate::log_utils::*;
 use hyper::Client;
 use hyper_tls::HttpsConnector;
-use futures::{
-    future::FutureExt, // for `.fuse()`
-    pin_mut,
-    select,
-};
+// use futures::{
+//     future::FutureExt, // for `.fuse()`
+//     pin_mut,
+//     select,
+// };
 use tokio::runtime::Runtime;
 use tokio::prelude::*;
+use rdkafka::message::OwnedHeaders;
+use rdkafka::producer::{FutureProducer, FutureRecord};
+
+const URL_TOPIC: &str = "urls";
 
 
 async fn get_page(url: &str, client: &Client<hyper_tls::client::HttpsConnector<hyper::client::connect::HttpConnector>>) -> Result<String, String> {
@@ -24,6 +44,34 @@ async fn get_page(url: &str, client: &Client<hyper_tls::client::HttpsConnector<h
     Ok(String::from(s))
 }
 
+
+// A context can be used to change the behavior of producers and consumers by adding callbacks
+// that will be executed by librdkafka.
+// This particular context sets up custom callbacks to log rebalancing events.
+struct CustomContext;
+
+impl ClientContext for CustomContext {}
+
+impl ConsumerContext for CustomContext {
+    fn pre_rebalance(&self, rebalance: &Rebalance) {
+        info!("Pre rebalance {:?}", rebalance);
+    }
+
+    fn post_rebalance(&self, rebalance: &Rebalance) {
+        info!("Post rebalance {:?}", rebalance);
+    }
+
+    // fn commit_callback(
+    //     &self,
+    //     result: KafkaResult<()>,
+    //     _offsets: *mut rdkafka_sys::RDKafkaTopicPartitionList,
+    // ) {
+    //     info!("Committing offsets: {:?}", result);
+    // }
+}
+
+// A type alias with your custom consumer can be created for convenience.
+type LoggingConsumer = StreamConsumer<CustomContext>;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
@@ -52,24 +100,99 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let (version_n, version_s) = get_rdkafka_version();
     info!("welcome to rust www crawler!");
     info!("using librdkafka version: 0x{:08x}, {}", version_n, version_s);
-    info!("cluster: {}", bootstrap_servers);
+    info!("bootstrap.servers: {}", bootstrap_servers);
 
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
 
+    let producer: FutureProducer = ClientConfig::new()
+        .set("bootstrap.servers", bootstrap_servers)
+        .set("message.timeout.ms", "5000")
+        .create()
+        .expect("Producer creation error");
+
+    let context = CustomContext;
+
+    let consumer: LoggingConsumer = ClientConfig::new()
+        .set("group.id", "url_cg")
+        .set("bootstrap.servers", bootstrap_servers)
+        .set("enable.partition.eof", "true")
+        .set("enable.auto.commit", "true")
+        // .set_log_level(RDKafkaLogLevel::Debug)
+        .create_with_context(context)
+        .expect("Consumer creation failed");
+
+    consumer
+        .subscribe(&[URL_TOPIC])
+        .expect("Can't subscribe to specified topics");
+
+    // seed with hacker news.
+    producer.send(FutureRecord::to(URL_TOPIC).payload("").key("https://news.ycombinator.com"), 0);
+
+    let mut message_stream = consumer.start().compat();
+
+    const MAX_INFLIGHT: usize = 10;
+
     let mut futs = vec![];
-    futs.push(Box::pin(get_page("http://confluent.io", &client)));
-    futs.push(Box::pin(get_page("https://www.matthowlett.com", &client)));
-    futs.push(Box::pin(get_page("https://news.ycombinator.com", &client)));
+    
 
     loop {
+        // if we don't have max requests in flight, or we haven't reached partition eof (currently assume 1 partition only: todo fix this),
+        // then keep consuming urls to read and add get_page tasks corresponding to them. 
+        // 
+        // if either of these conditions fails, then switch to waiting on get page completions.
+        // 
+        // this is a bit flawed.
+        loop {
+            let mut eof = false;
+            let mut url = "";
+
+            if let Some(message) = message_stream.next().await {
+                match message {
+                    Err(_) => warn!("Error while reading from stream."),
+                    Ok(Err(e)) => {
+                        if let rdkafka::error::KafkaError::PartitionEOF(_) = e {
+                            eof = true;
+                            info!("eof");
+                        } else {
+                            warn!("consume error");
+                        }
+                    },
+                    Ok(Ok(msg)) => {
+                        let aaa = msg.key();
+                        let aa = match aaa {
+                            None => {
+                                warn!("no key");
+                            },
+                            Some(bs) => {
+                                let b = std::str::from_utf8(&bs)?;
+                                url = b.clone();
+                            }
+                        };
+                    }
+                };
+            } else {
+                warn!("unexpected, I think");
+            }
+
+            if eof || futs.len() >= MAX_INFLIGHT {
+                break;
+            }
+
+            futs.push(Box::pin(get_page(url, &client)));
+        }
+
         let mut idx = 0;
         if futs.len() == 0 { break; }
         {
             let ff = futures::future::select_all(futs.iter_mut()).await;
             idx = ff.1;
             match ff.0 {
-                Ok(r) => println!("{}", r),
+                Ok(r) => {
+                    println!("{}", r);
+                    // get page urls here.
+                    producer.send(FutureRecord::to(URL_TOPIC).payload("").key("https://news.ycombinator.com"), 0);
+                },
                 Err(_) => println!("no result")
             }
         }
@@ -80,4 +203,3 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     Ok(())
 }
-
